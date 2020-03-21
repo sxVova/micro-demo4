e56f05859459504edcbf314316387f89e8278c6c
sxVova <53950314+sxVova@users.noreply.github.com>
2020-03-21 13:10:46 +0200

    Update tag-changer.sh

--- tag-changer.sh
+++ tag-changer.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
-
+TAG=$1
 #emailservice-changer
-sed "s,\image: gcr.io/devops-259416/emailservice:..*,\gcr.io/devops-259416/emailservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/emailservice:..*,\gcr.io/devops-259416/emailservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #checkoutservice-changer
-sed "s,\image: gcr.io/devops-259416/checkoutservice:..*,\gcr.io/devops-259416/checkoutservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/checkoutservice:..*,\gcr.io/devops-259416/checkoutservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #recommendationservice-changer
-sed "s,\image: gcr.io/devops-259416/recommendationservice:..*,\gcr.io/devops-259416/recommendationservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/recommendationservice:..*,\gcr.io/devops-259416/recommendationservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #frontend-changer
-sed "s,\image: gcr.io/devops-259416/frontend:..*,\gcr.io/devops-259416/frontend: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/frontend:..*,\gcr.io/devops-259416/frontend: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #paymentservice-changer
-sed "s,\image: gcr.io/devops-259416/paymentservice:..*,\gcr.io/devops-259416/paymentservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/paymentservice:..*,\gcr.io/devops-259416/paymentservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #productcatalogservice-changer
-sed "s,\image: gcr.io/devops-259416/productcatalogservice:..*,\gcr.io/devops-259416/productcatalogservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/productcatalogservice:..*,\gcr.io/devops-259416/productcatalogservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #cartservice-changer
-sed "s,\image: gcr.io/devops-259416/cartservice:..*,\gcr.io/devops-259416/cartservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/cartservice:..*,\gcr.io/devops-259416/cartservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #loadgenerator-changer
-sed "s,\image: gcr.io/devops-259416/loadgenerator:..*,\gcr.io/devops-259416/loadgenerator: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/loadgenerator:..*,\gcr.io/devops-259416/loadgenerator: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #currencyservice-changer
-sed "s,\image: gcr.io/devops-259416/currencyservice:..*,\gcr.io/devops-259416/currencyservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/currencyservice:..*,\gcr.io/devops-259416/currencyservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #adservice-changer
-sed "s,\image: gcr.io/devops-259416/adservice:..*,\gcr.io/devops-259416/adservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/adservice:..*,\gcr.io/devops-259416/adservice: $TAG,g" -i release/kubernetes-manifests.yaml
 
 #shippingservice-changer
-sed "s,\image: gcr.io/devops-259416/shippingservice:..*,\gcr.io/devops-259416/shippingservice: $SOMETHING,g" -i release/kubernetes-manifests.yaml
+sed "s,\image: gcr.io/devops-259416/shippingservice:..*,\gcr.io/devops-259416/shippingservice: $TAG,g" -i release/kubernetes-manifests.yaml
